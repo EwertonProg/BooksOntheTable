7339099346e682bae3410a8bfdafd87b91b37743
Ewerton Luis <ewerton.lsantana@gmail.com>
2021-07-18 00:42:55 -0300

    update

--- app/src/main/java/com/android/ewerton/booksonthetable/app/BooksOnTheTableApp.kt
+++ app/src/main/java/com/android/ewerton/booksonthetable/app/BooksOnTheTableApp.kt
@@ -5,12 +5,12 @@ import com.android.ewerton.booksonthetable.di.module.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class BooksOnTheTableApp :Application(){
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger()
             androidContext(this@BooksOnTheTableApp)
             modules(
                 databaseModule,
